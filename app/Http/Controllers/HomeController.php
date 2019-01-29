5f298125c4232533746c97dff827105696ed4799
yaimsc <yaimsc@gmail.com>
2019-01-29 11:38:26 +0100

    api con token get

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,10 +36,37 @@ class HomeController extends Controller
               dump($json[$i]->title);
             }
           }
+
+          $curl = curl_init();
+
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.imgur.com/3/account/YaizaM",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => false,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+              "Authorization: Bearer 1d39a5280c88d9d4413f681049b910184c8ef19a"
+            ),
+          ));
+
+          $response = curl_exec($curl);
+          $err = curl_error($curl);
+
+          curl_close($curl);
+
+          if ($err) {
+            echo "cURL Error #:" . $err;
+          } else {
+            echo $response;
+          }
     }
 
-    /**
-     * Show the form for creating a new resource.
+
+     /* Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
@@ -56,7 +83,7 @@ class HomeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
     }
 
     /**
